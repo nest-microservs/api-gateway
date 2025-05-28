1a1bd38ab0dd09f03921619258656f516e486af2
carlosbult <acevedo.bult@gmail.com>
2025-05-27 20:50:41 -0400

    Basic env config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { envs } from "./config";
+import { Logger, ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
+  const logger = new Logger("Api-Gateway");
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix("api");
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(envs.port);
+  logger.log(`Api-Gateway is running on port ${envs.port}`);
 }
 bootstrap();
---
+++ src/users/users.controller.ts
@@ -0,0 +1,35 @@
+import { Controller } from "@nestjs/common";
+import { MessagePattern, Payload } from "@nestjs/microservices";
+import { UsersService } from "./users.service";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
+
+@Controller()
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @MessagePattern({ cmd: "createUser" })
+  create(@Payload() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @MessagePattern({ cmd: "findAllUsers" })
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @MessagePattern({ cmd: "findOneUser" })
+  findOne(@Payload() id: number) {
+    return this.usersService.findOne(id);
+  }
+
+  @MessagePattern("updateUser")
+  update(@Payload() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(updateUserDto.id, updateUserDto);
+  }
+
+  @MessagePattern({ cmd: "removeUser" })
+  remove(@Payload() id: number) {
+    return this.usersService.remove(id);
+  }
+}
